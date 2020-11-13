38b58188acd038946687cf9e29d3655572d1bac5
Erick Surya Dinata <ericksuryadinata@gmail.com>
2020-11-13 19:10:37 +0700

    cek jika service worker sudah aktif atau belum

--- sw-register.js
+++ sw-register.js
@@ -37,17 +37,19 @@ if ('Notification' in window) {
   console.error('Browser tidak mendukung notifikasi.') // eslint-disable-line no-console
 }
 
-if (('PushManager' in window)) {
-  navigator.serviceWorker.getRegistration().then(registration => {
-    registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array('BMCV2ji6nmg6kFQrvJ9_gnkM0WuatBIlEj5nN2vM3CSDk40EbuD9mKLLYoZ851ZEDxwmkpGTrxFbMEgm_qCOrtM') // eslint-disable-line no-undef
-    }).then(subscribe => {
-      console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint) // eslint-disable-line no-console
-      console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey('p256dh'))))) // eslint-disable-line no-console
-      console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey('auth'))))) // eslint-disable-line no-console
-    }).catch(e => {
-      console.error('Tidak dapat melakukan subscribe ', e.message) // eslint-disable-line no-console
+navigator.serviceWorker.ready.then(() => {
+  if (('PushManager' in window)) {
+    navigator.serviceWorker.getRegistration().then(registration => {
+      registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array('BMCV2ji6nmg6kFQrvJ9_gnkM0WuatBIlEj5nN2vM3CSDk40EbuD9mKLLYoZ851ZEDxwmkpGTrxFbMEgm_qCOrtM') // eslint-disable-line no-undef
+      }).then(subscribe => {
+        console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint) // eslint-disable-line no-console
+        console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey('p256dh'))))) // eslint-disable-line no-console
+        console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey('auth'))))) // eslint-disable-line no-console
+      }).catch(e => {
+        console.error('Tidak dapat melakukan subscribe ', e.message) // eslint-disable-line no-console
+      })
     })
-  })
-}
+  }
+})
